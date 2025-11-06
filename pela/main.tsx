6b9dc6c419fc7a28b4939e94466e832fd257101e
Roger Purtsak <rogerpurtsak10@gmail.com>
2025-11-06 16:44:42 +0200

    css was missing

--- pela/main.tsx
+++ pela/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
 import { VenueAdmin } from './components/VenueAdmin'
+import './styles/globals.css'
 
 const router = createBrowserRouter([
   { path: '/', element: <App /> },
